22389dac3630b3d32cb5df44346bf6ba3f290dc6
Ananas-Prog <anais.mottier.etu@univ-lemans.fr>
2020-12-02 19:05:09 +0100

    encodageHDBn

--- codeHDBn.c
+++ codeHDBn.c
@@ -1,50 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int encodeurHDBn_Arithmetique(int reset, int data, int p, int n){
+void afficherHDBn(int tab[23], int nb){
+  for(int i=0; i<23; i++){
+    printf("%d ", tab[i]);
+  }
+  printf("\n");
+}
+
+int encodeurHDBn_Arithmetique(int reset[23], int data[23], int p, int n, int nb){
   int dernierViol = -1;
   int dernierUn = -1;
   int nbZero = 0;
+  int i;
 
-  for(int i=0; !data; i++){
-    if(data[i]==0){
+  for(i=0; i<23; i++){
+    if(data[i] == 0){
       nbZero++;
-      if(nbZero > n){ //trop de zero à la suite
+      if(nbZero == nb+1){ //trop de zero à la suite
         if(dernierViol > 0){ //dernierViol positif
+          reset[i] = -1; //V-
           if(dernierUn > 0){ //dernierUn positif
-            reset[i]=-1;
+            reset[i-nb] = -1; //B-
+            dernierUn = reset[i-nb];
           }
-          else{ //dernierUn negatif
-            reset[i]=1;
-          }
-          dernierUn*=-1;
         }
         else{ //dernierViol négatif
-
+          reset[i] = 1; //V+
+          if(dernierUn < 0){ //dernierUn négatif
+            reset[i-nb] = 1; //B+
+            dernierUn = reset[i-nb];
+          }
         }
+        dernierViol = reset[i];
+        nbZero=0;
+      }
+      else{
+        reset[i] = 0;
       }
     }
+
     else{ //bite == 1 : code bipolaire
+      nbZero=0;
       if(dernierUn < 0){ ///dernierUn negatif
         reset[i]=1;
       }
       else{//dernierUn positif
         reset[i]=-1;
       }
-      dernierUn*=-1;
+      dernierUn = reset[i];
     }
   }
+
+  afficherHDBn(reset, nb);
 }
 
 int main(){
   int p, n, nb;
-  print("Veuillez saisir la valeur de n : ");
-  scanf("%d\n", &nb);
+  printf("Veuillez saisir la valeur de n : ");
+  scanf("%d", &nb);
 
   //int * data = malloc(sizeof(int)*23)
-  int data = [1,0,0,1,0,1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,1,0,0];
-  int reset[data.length];
-  encodeurHDBn_Arithmetique(reset, data, p, n);
+  int data[] = {1,0,0,1,0,1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,1,0,0};
+  int reset[23];
+  encodeurHDBn_Arithmetique(reset, data, p, n, nb);
 
   float f;
 }
